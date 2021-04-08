4517d4da97cadfecced74e073a4444c32fc81518
paulvmking <paulvmking@gmail.com>
2021-04-08 22:08:18 +0000

    added remainder of text to allow flask app to run

--- run.py
+++ run.py
@@ -1,4 +1,20 @@
+import os
 from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello, World"
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
 
 
 
