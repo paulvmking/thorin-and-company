dfee6a98f85c09e451ffd8575e047c25bbcb4cf2
paulvmking <paulvmking@gmail.com>
2021-04-09 20:38:29 +0000

    added routing for contact page

--- run.py
+++ run.py
@@ -9,9 +9,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
