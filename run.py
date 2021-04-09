21781d0fbc31cd49d87375861465ba186071fec4
paulvmking <paulvmking@gmail.com>
2021-04-09 19:42:04 +0000

    added template and files to render about and home page with flask

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
