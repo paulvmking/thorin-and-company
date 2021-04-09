23ac53d5ff6df049b451c35fb5cf242b71597091
paulvmking <paulvmking@gmail.com>
2021-04-09 20:50:27 +0000

    added careers page

--- run.py
+++ run.py
@@ -20,6 +20,11 @@ def contact():
     return render_template("contact.html")
 
 
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+    
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
