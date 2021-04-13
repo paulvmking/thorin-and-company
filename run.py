8456ec0ffa07a20b8b02c5144e85cf0ebe7d7c3e
paulvmking <paulvmking@gmail.com>
2021-04-13 15:58:51 +0000

    added flash to contact function

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import os
 import json
 from flask import Flask, render_template, request, flash
-
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -33,8 +35,8 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form.get("name"))
-        print(request.form["email"])
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
